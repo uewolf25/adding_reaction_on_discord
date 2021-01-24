da1a4b602b972e1b550a0c0fb56ab30342096565
wolf <nyan643.ym25mm10@gmail.com>
2021-01-24 18:13:30 +0900

    add: emoji list for reactions

---
+++ emoji_list_file.py
@@ -0,0 +1,51 @@
+#!/.anyenv/envs/pyenv/shims/python
+# -*- coding: utf-8 -*-
+
+bananas = [
+  "\N{Banana}"
+  "\N{Splashing Sweat Symbol}"
+]
+
+# 絵文字(最大付与数：20)
+emoji_list = [
+  "\N{Banana}", 
+  "\N{Splashing Sweat Symbol}",
+  "\N{Comet}",
+  "\N{Clapping Hands sign}",
+  "\N{Eyes}",
+  "\N{Dog}",
+  "\N{Hear-No-Evil Monkey}",
+  "\N{Steaming Bowl}",
+  "\N{sauropod}",
+  "\N{Turtle}",
+  "\N{Grinning Face}",
+  "\N{Smiling Face with Open Mouth}",
+  "\N{Smiling Face with Open Mouth and Smiling Eyes}",
+  "\N{Grinning Face with Smiling Eyes}",
+  "\N{Smiling Face with Open Mouth and Tightly-Closed Eyes}",
+  "\N{Smiling Face with Open Mouth and Cold Sweat}",
+  "\N{Face with Tears of Joy}",
+  "\N{Slightly Smiling Face}",
+  "\N{Upside-Down Face}",
+  "\N{Winking Face}",
+  # "\N{Smiling Face with Smiling Eyes}",
+  # "\N{Smiling Face with Halo}",
+  # "\N{Smiling Face with Smiling Eyes and Three Hearts}",
+  # "\N{Smiling Face with Heart-Shaped Eyes}",
+  # "\N{Grinning Face with Star Eyes}",
+  # "\N{Face Throwing a Kiss}",
+  # "\N{Kissing Face}",
+  # "\N{White Smiling Face}",
+  # "\N{Kissing Face with Closed Eyes}",
+  # "\N{Kissing Face with Smiling Eyes}",
+  # # "\N{Smiling Face with Tear}",
+  # "\N{Face Savouring Delicious Food}",
+  # "\N{Face with Stuck-Out Tongue}",
+  # "\N{Face with Stuck-Out Tongue and Winking Eye}",
+  # "\N{Grinning Face with One Large and One Small Eye}",
+  # "\N{Money-Mouth Face}",
+  # "\N{Hugging Face}",
+  # "\N{Smiling Face with Smiling Eyes and Hand Covering Mouth}",
+  # "\N{}",
+]
+
